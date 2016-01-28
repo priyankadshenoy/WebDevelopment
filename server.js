b5b413629da67357f3c55ea41ff50fa4f990863e
shenoyp <shenoyp@ccs.neu.edu>
2016-01-28 13:40:16 -0500

    test

--- server.js
+++ server.js
@@ -3,16 +3,4 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-app.get('/hello', function(req, res){
-    res.send('hello world hello');
-});
-app.get('/send/users', function(req, res){
-    var users=[
-        {username: 'adeles', first: 'adele', last: 'sheldon'},
-        {username: 'sheldon', first: 'sheldon', last: 'cooper'}
-    ];
-    res.json(users);
-});
-
-app.listen(port,ipaddress);
+app.listen(port, ipaddress);
